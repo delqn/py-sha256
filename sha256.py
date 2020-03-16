56ee497762e39a1c1ce774278063aa4acc08edd4
nicolas3355 <nicolaselhaddad.nh@gmail.com>
2020-03-16 19:47:25 -0400

    porting code to python3

--- sha256.py
+++ sha256.py
@@ -3,7 +3,6 @@
 """
 Symbols and Operations
     The following symbols are used in the secure hash algorithm specifications; each operates on w-bit words:
-
     ∧ Bitwise AND operation.
     ∨ Bitwise OR (“inclusive-OR”) operation.
     ⊕ Bitwise XOR (“exclusive-OR”) operation.
@@ -61,7 +60,6 @@ which are represented as x, y, and z. The result of each function is a new 64-bi
 
 def choose(x, y, z):
     """The x input chooses if the output is from y or z.
-
     Ch(x,y,z)=(x∧y)⊕(¬x∧z)
     """
     return z ^ (x & (y ^ z))
@@ -69,9 +67,7 @@ def choose(x, y, z):
 
 def majority(x, y, z):
     """The result is set according to the majority of the 3 inputs.
-
     Maj(x, y,z) = (x ∧ y) ⊕ (x ∧ z) ⊕ ( y ∧ z)
-
     The functions are defined for bit vectors (of 32 bits in case fo SHA-256)
     """
     return ((x | y) & z) | (x & y)
@@ -118,12 +114,12 @@ def mutate(data, digest):
         sum_ = gamma1(w[i - 2]) + w[i - 7] + gamma0(w[i - 15]) + w[i - 16]
         w.append(sum_ & 0xffffffff)
 
-    for idx in xrange(0, -64, -1):
+    for idx in range(0, -64, -1):
         i = abs(idx % 8)
 
         # Initialize the eight working variables, a, b, c, d, e, f, g, and h  with the (i-1)st hash value.
         # W is the prepared message schedule.
-        positions = [(i + x) % 8 for x in xrange(8)]
+        positions = [(i + x) % 8 for x in range(8)]
         d_position = positions[3]
         h_position = positions[-1]
         a, b, c, d, e, f, g, h = [digest_copy[pos] for pos in positions]
@@ -138,13 +134,8 @@ def mutate(data, digest):
 
 def get_buffer(s):
     if isinstance(s, str):
-        return s
-    if isinstance(s, unicode):
-        try:
-            return str(s)
-        except UnicodeEncodeError:
-            pass
-    return buffer(s)
+        return s.encode('utf-8')
+    return memoryview(s)
 
 
 def zeros(count):
@@ -161,6 +152,7 @@ class SHA256(object):
         }
         if not string:
             return
+
         buff = get_buffer(string)
         count = len(buff)
         count_lo = (self._sha['count_lo'] + (count << 3)) & 0xffffffff
@@ -171,12 +163,12 @@ class SHA256(object):
 
         buffer_idx = 0
         while count >= BLOCK_SIZE:
-            self._sha['data'] = [ord(c) for c in buff[buffer_idx:buffer_idx + BLOCK_SIZE]]
+            self._sha['data'] = [c for c in buff[buffer_idx:buffer_idx + BLOCK_SIZE]]
             count -= BLOCK_SIZE
             buffer_idx += BLOCK_SIZE
             self._sha['digest'] = mutate(self._sha['data'], self._sha['digest'])
 
-        self._sha['data'][:count] = [ord(c) for c in buff[buffer_idx:buffer_idx + count]]
+        self._sha['data'][:count] = [c for c in buff[buffer_idx:buffer_idx + count]]
 
     def hexdigest(self):
         """
@@ -197,28 +189,34 @@ class SHA256(object):
         else:
             hash['data'] = hash['data'][:count] + zeros(BLOCK_SIZE - count)
 
-        for idx, shift in zip(range(56, 64), range(24, -1, -8) * 2):
+        for idx, shift in zip(range(56, 64), list(range(24, -1, -8)) * 2):
             hash['data'][idx] = (hash['count_hi' if idx < 60 else 'count_lo'] >> shift) & 0xff
 
         hash['digest'] = mutate(hash['data'], hash['digest'])
 
         digest = []
         for i in hash['digest']:
-            for shift in xrange(24, -1, -8):
+            for shift in range(24, -1, -8):
                 digest.append((i >> shift) & 0xff)
         return ''.join(['%.2x' % i for i in digest[:DIGEST_SIZE]])
 
 
 def test():
+    import hashlib
+    import random
+    import string
+    for i in range(0, 1000):
+        random_string_function = lambda x: ''.join([random.choice(string.printable) for i in range(random.randint(1, x))])
+        rnd = random_string_function(random.randint(1, 100))
+        assert hashlib.sha256(rnd.encode()).hexdigest() == SHA256(rnd).hexdigest() 
     string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     assert 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' == SHA256().hexdigest()
     assert 'a58dd8680234c1f8cc2ef2b325a43733605a7f16f288e072de8eae81fd8d6433' == SHA256(string).hexdigest()
     assert 'db7b94909697ac91e9f167159b99a1d2612b5cf4b3086a72cb6ac0206c4bd47c' == SHA256(string * 7).hexdigest()
-    assert '1aa4458852eefd69560827a035db9df11491abdae3483a71d1707f05e085e682' == SHA256('hello⊕').hexdigest()
+    assert '1aa4458852eefd69560827a035db9df11491abdae3483a71d1707f05e085e682' == SHA256('hello⊕'.encode('utf-8')).hexdigest()
     long_text = string * 999
     assert '5e4e5fcc4c89b7b1b6567d81187e83c99cd7c04ca77a093ed74e35a08046d519' == SHA256(long_text).hexdigest()
-    assert 'c7ae9b6438e9dfccfd486fabed3c08d6f63ae559ef09b2fe084a38dbc46fae7c' == SHA256(u'\uE52D').hexdigest()
-    print 'ok'
+    print ('ok')
 
 
 if __name__ == "__main__":
